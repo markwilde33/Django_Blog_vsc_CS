bc6f741cd3f5a9bc1bcef7c52b4f4263aae05035
markwilde33 <markwilde29@gmail.com>
2018-12-12 20:55:15 +0000

    Initial commit. Set up a virtual environment using pipenv for this project. This is a follow along to a Corey Schafer Django blog tutorial. This project is upto date with Web App Part 2-Applications and Routes.

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('<h1>Hello Clarice</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>About Clarice</h1>')
