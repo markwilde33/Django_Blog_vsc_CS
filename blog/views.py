dd3700fea476e5fc8819046e0ba85fd3fab3518f
markwilde33 <markwilde29@gmail.com>
2018-12-14 20:59:03 +0000

    Upto date with Full-Featured Web App Part 5 - Database and Migrations

--- blog/views.py
+++ blog/views.py
@@ -1,34 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-posts = [
-
-    {
-
-        'author': 'sparks',
-        'title': 'Blog Post 1',
-        'content': 'Pish',
-        'date_posted': 'December 12, 3939',
-        'ps': 'Keep fighting'
-
-    },
-
-    {
-
-        'author': 'muffy',
-        'title': 'Blog Post 2',
-        'content': 'Sham',
-        'date_posted': 'November 25, 4000',
-        'ps': 'no reteat no surrender'
-
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
